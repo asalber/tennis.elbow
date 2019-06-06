699cc00699dcb3b2de1269d8321232bfa73ac026
Alfredo Sánchez Alberca <asalber@gmail.com>
2019-06-06 13:03:41 +0200

    Cambio semilla.

--- inst/rkward/plugins/homework.js
+++ inst/rkward/plugins/homework.js
@@ -8,7 +8,7 @@ function preprocess(){
 function calculate () {
 	dni = getString("dni");
 	echo('data(tennis.elbow, package="tennis.elbow")\n');
-	echo('set.seed(' + dni + ')\n');
+	echo('set.seed(' + dni + '1)\n');
         echo('tennis.elbow$Age <- tennis.elbow$Age + round(rnorm(length(tennis.elbow$Pain.Relief.Max),0,1))\n');
 	echo('tennis.elbow$Pain.Relief.Max <- tennis.elbow$Pain.Relief.Max + round(rnorm(length(tennis.elbow$Pain.Relief.Max),0,0.25))\n');
         echo('tennis.elbow$Pain.Relief.12h <- tennis.elbow$Pain.Relief.12h + round(rnorm(length(tennis.elbow$Pain.Relief.12h),0,0.25))\n');
@@ -20,10 +20,10 @@ function calculate () {
         echo('  return(x)\n');
         echo('}\n');
         echo('fun <- function(x) bound(x,1,6)\n');
-        echo('sapply(tennis.elbow$Pain.Relief.Max,fun)\n');
-        echo('sapply(tennis.elbow$Pain.Relief.12h,fun)\n');
-        echo('sapply(tennis.elbow$Pain.Relief.24h,fun)\n');
-        echo('sapply(tennis.elbow$Pain.Relief.Global,fun)\n');
+        echo('tennis.elbow$Pain.Relief.Max <- sapply(tennis.elbow$Pain.Relief.Max,fun)\n');
+        echo('tennis.elbow$Pain.Relief.12h <- sapply(tennis.elbow$Pain.Relief.12h,fun)\n');
+        echo('tennis.elbow$Pain.Relief.24h <- sapply(tennis.elbow$Pain.Relief.24h,fun)\n');
+        echo('tennis.elbow$Pain.Relief.Global <- sapply(tennis.elbow$Pain.Relief.Global,fun)\n');
 	echo('.GlobalEnv$tennis.elbow.' + dni + '<- tennis.elbow\n');
 	echo('rm(tennis.elbow,envir=.GlobalEnv)\n');
 }
