fa1a684ed36402c706196779d2a10ba4780eebd7
Alfredo Sánchez Alberca <asalber@gmail.com>
2022-06-19 08:27:52 +0200

    Seed update.

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Tennis elbow injury
+#'
+#' A data frame with 87 observations about the tennis elbow injury.
+#'
+#' @format A data frame with 87 observations and 8 variables:
+#' \describe{
+#' \item{\code{Age}}{a numeric vector with the age}
+#' \item{\code{Gender}}{a factor with the gender}
+#' \item{\code{Treatment}}{a factor with the treatment}
+#' \item{\code{Gender.Treatment}}{a factor with the categories resulting of combining the gender and the treatment}
+#' \item{\code{Pain.Relief.Max}}{a numeric vector with the pain relief at the time of maximum effort (scale 1-6)}
+#' \item{\code{Pain.Relief.12h}}{a numeric vector with the pain relief after 12 hours of making the effort (scale 1-6)}
+#' \item{\code{Pain.Relief.24h}}{a numeric vector with the pain relief after 24 hours of making the effort (scale 1-6)}
+#' \item{\code{Pain.Relief.Global}}{a numeric vector with the pain relief feeling during and after the effort (scale 1-6)}
+#' }
+"tennis.elbow"
